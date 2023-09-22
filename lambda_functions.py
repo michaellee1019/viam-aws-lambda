c831079d63aee8385eb7433e4de93d1c39a780d7
Michael Lee <michaellee1019@gmail.com>
2023-09-22 09:14:17 -0400

    Updates for tutorial

--- lambda_functions.py
+++ lambda_functions.py
@@ -1,9 +1,11 @@
+import os
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 import asyncio
+import time
 
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
@@ -13,32 +15,55 @@ import boto3
 secrets = boto3.client('ssm')
 import json
 
+def printer_clear(event, context):
+    for robot in event.get("robots"):
+        asyncio.run(printer_event_main('        '), robot)
+
 def printer_event(event, context):
     print(event)
-    progress = event.get('progress', dict()).get('completion')
-    return asyncio.run(printer_event_main('UKN' if progress is None else progress))
 
-async def printer_event_main(progress: str):
-    location_secret = secrets.get_parameter(Name='/printerstatus/location-secret', WithDecryption=True)
-    robot_fqdn = secrets.get_parameter(Name='/printerstatus/robot-fqdn', WithDecryption=True)
-    robot = await connect(location_secret, robot_fqdn)
-    led_segment = Generic.from_robot(robot, "segments")
-    await led_segment.do_command({"print":{"value":"PRINT" + progress}})
+    os.environ['TZ'] = 'EST+05EDT'
+    time.tzset()
+    print("it is now: ",time.localtime())
+    if time.localtime().tm_hour > (9+12):
+        print("sleeping because its past 10pm")
+        asyncio.run(printer_event_main('        '))
 
-    await robot.close()
+    message = 'UNK. STAT.'
+    topic = event.get('topic')
+    if topic == 'User Action Needed':
+        message = 'HELP NOW'
+    else:
+        progress = "{:.0f}".format(event.get('progress', dict()).get('completion')).rjust(3,' ')
+        message = "PRINT" + progress
+    for robot in event.get("robots"):
+        asyncio.run(printer_event_main(message, robot))
+
+async def printer_event_main(progress: str, robot: str):
+    try:
+        location_secret = secrets.get_parameter(Name="/{0}/location-secret".format(robot), WithDecryption=True)
+        robot_fqdn = secrets.get_parameter(Name='/{0}/robot-fqdn'.format(robot), WithDecryption=True)
+        robot = await connect(location_secret, robot_fqdn)
+        led_segment = Generic.from_robot(robot, "led_displays")
+        await led_segment.do_command({"print":{"value":progress}})
+        await robot.close()
+    except Exception as ex:
+        print(ex)
+        return {'result': "failure"}
 
     response = {'result': "success"}
     return response
 
 def chachinga(event, context):
-    asyncio.run(chachinga_main())
+    print(event)
+    asyncio.run(chachinga_main(event.get('sound')))
 
-async def chachinga_main():
+async def chachinga_main(sound: str):
     location_secret = secrets.get_parameter(Name='/chachinga/location_secret', WithDecryption=True)
     robot_fqdn = secrets.get_parameter(Name='/chachinga/robot_fqdn', WithDecryption=True)
     robot = await connect(location_secret, robot_fqdn)
     speaker = Generic.from_robot(robot, "speaker")
-    await speaker.do_command({"play":{"sound":"chaching"}})
+    await speaker.do_command({"play":{"sound":sound}})
 
     await robot.close()
 
@@ -56,4 +81,4 @@ async def connect(location_secret, robot_fqdn):
     return await RobotClient.at_address(robot_fqdn['Parameter']['Value'], opts)
 
 if __name__ == '__main__':
-    asyncio.run(printer_event_main())
+    asyncio.run(printer_event_main("TESTING1","printerstatus"))
